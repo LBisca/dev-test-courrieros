eeffa850eb294fe26ba0301ad26cd0e1678379bb
LBisca <lucas.simon.b@gmail.com>
2018-07-20 14:53:50 -0300

    Add Connect CsvReader to android framework

--- app/src/main/java/com/courrieros/MainActivity.kt
+++ app/src/main/java/com/courrieros/MainActivity.kt
@@ -2,12 +2,16 @@ package com.courrieros
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.courrieros.reader.CsvReader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text =  CsvReader().readInput(resources.openRawResource(R.raw.client_orders))
     }
 }
 
--- app/src/main/java/com/courrieros/reader/CsvReader.kt
+++ app/src/main/java/com/courrieros/reader/CsvReader.kt
@@ -3,13 +3,11 @@ package com.courrieros.reader
 import java.io.*
 
 class CsvReader {
-    fun readInput(inputStream: InputStream) {
+    fun readInput(inputStream: InputStream): String {
         val reader = InputStreamReader(inputStream)
         val bufferedReader = BufferedReader(reader)
 
-        val text = read(bufferedReader, "")
-        println(text)
-
+        return read(bufferedReader, "")
     }
 
     private fun read(bufferedReader: BufferedReader, accumulator: String): String {
