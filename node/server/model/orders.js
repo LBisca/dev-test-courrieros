57d2b175aa12b5ddf014be89acc52924139d7619
LBisca <lucas.simon.b@gmail.com>
2018-07-23 23:06:58 -0300

    Add  server connection"

---
+++ node/server/model/orders.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+
+var Order = mongoose.model('company', {
+    companyName: {
+        type: String,
+        required: true
+    },
+    order: {
+        type: String,
+        required: true
+    },      
+    distance: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = {Order}
---
+++ node/server/server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+
+var {mongoose} = require('./db/mongoose')
+var {Order} = require('./model/orders')
+
+var app = express()
+const port = process.env.PORT || 3000
+
+app.get('/client/total', (req, res) => {
+        Order.find().then((company) => {
+        res.send({company})
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Starting at the ${port} port`)
+})
